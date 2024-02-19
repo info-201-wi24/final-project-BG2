4c646a40bcc63987e28e6de0a4a5bc2d395c3131
ashtraiy <148135066+ashtraiy@users.noreply.github.com>
2024-02-19 09:06:55 -0800

    Update Social Media and Mental Health.R

--- Social Media and Mental Health.R
+++ Social Media and Mental Health.R
@@ -1,6 +1,14 @@
 library(readr)
 library(readxl)
-Social_Media_Mental_Health <- read_csv("C:/Users/Thien/Downloads/archive/smmh.csv")
-Digital_Behavior_and_Mental_Health_Survey_2022 <- read_excel("C:/Users/Thien/Downloads/mental health and social media/mental health and social media/Digital Behavior and Mental Health Survey 2022.xlsx")
+library(dplyr)
+Social_Media_Mental_Health <- read_csv("smmh.csv")
+Digital_Behavior_and_Mental_Health_Survey_2022 <- read_excel("Digital Behavior and Mental Health Survey 2022.xlsx")
 
-Social_Media_Mental_Health$`9. How often do you find yourself using Social media without a specific purpose?` <- NULL
+
+#Cleaning data to relevant subjects 
+Social_Media_Mental_Health <- Social_Media_Mental_Health[-c(1, 3, 4, 6, 12, 15, 16, 19)]
+Digital_Behavior_and_Mental_Health_Survey_2022  <- Digital_Behavior_and_Mental_Health_Survey_2022 [-c(1)]
+
+# Combining datasets
+merged_dataset <- bind_rows(Social_Media_Mental_Health, Digital_Behavior_and_Mental_Health_Survey_2022)
+
