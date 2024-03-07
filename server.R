47c07b59931717691589b1df033ceb2e9512f50a
ashtraiy <148135066+ashtraiy@users.noreply.github.com>
2024-03-06 22:13:17 -0800

    final
    final change

--- server.R
+++ server.R
@@ -10,23 +10,25 @@ library(pheatmap)
 
 
 
-
+#reading crap in, we included a merged data set and we used it!!
 smmh <- read_csv("smmh.csv", show_col_types = FALSE)
 data <- read_csv("merged_data_final.csv", show_col_types = FALSE)
-
-
+# Defined the server logic for the Shiny app
 server <- function(input, output) {
   
-  #session
-  #-------------------------------------------------------------------------------------------------------------------------------------------------
-  # Page 1 - start
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+# Page 1 - start
   
+  # Render a plot in the UI output placeholder 'variableDistributionPlot'
   output$variableDistributionPlot <- renderPlot({
+    
+    # Filter 'smmh' data based on age range and social media usage inputs
     filtered_data <- smmh %>%
       filter(`1. What is your age?` >= input$ageRange[1],
              `1. What is your age?` <= input$ageRange[2],
              `8. What is the average time you spend on social media every day?` == input$socialMediaTimeSpent)
     
+    # Create and return a histogram of filtered data with aesthetic mappings and styles
     ggplot(filtered_data, aes(x = `1. What is your age?`)) +
       geom_histogram(binwidth = 1, fill = "red", color = "black") +
       labs(x = "Age", y = "Social Media Users") +
@@ -41,11 +43,13 @@ server <- function(input, output) {
 # Page 1 - end 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 
-  
+# you dont have to use the same code but remeber to add the github and stack overflow citation
   
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
   # Page 2 - begin 
+  
+# Renders yet another plot in the UI output placeholder 'plot'
 output$plot <- renderPlot({
   filtered_data <- if (input$gender == "All") {
     data
@@ -54,15 +58,18 @@ output$plot <- renderPlot({
       dplyr::filter(Gender == input$gender)
   }
   
+  # Begin constructing the plot with age on x-axis and style it
   p <- ggplot(filtered_data) + theme_minimal() + 
     labs(x = "Age", y = "Score")
 
+  # Add self-reported mental health status points to the plot if selected
   if (input$selfReportedCheck && "Self-reported Mental Health Status" %in% names(filtered_data)) {
     p <- p + geom_point(data = filtered_data %>% filter(!is.na(`Self-reported Mental Health Status`)), 
                         aes(x = Age, y = `Self-reported Mental Health Status`), 
                         color = "blue")
   }
   
+  # Add actual mental health score points to the plot if selected
   if (input$actualScoreCheck && "Impact on Mental Health (Score)" %in% names(filtered_data)) {
     p <- p + geom_point(data = filtered_data %>% filter(!is.na(`Impact on Mental Health (Score)`)), 
                         aes(x = Age, y = `Impact on Mental Health (Score)`), 
@@ -74,29 +81,38 @@ output$plot <- renderPlot({
   
 # Page 2 - end
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
-
   
+# use reactive for this one
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
-    # Page 3 - begin 
+# Page 3 - begin 
     
-    output$myImage <- renderImage({
-      list(src = "IMG_3156.WEBP",
-           align = "center",
-           width = "100%",
-           height = 500)
-    }, deleteFile = F)
-    filtered_data <-reactive({
-      merged_dataset
-    })
-    filtered_smmh <-reactive({
-      smmh[smmh$`3. Relationship Status` %in% input$relationship_status &
-             smmh$`8. What is the average time you spend on social media every day?` == input$time_spent, ]
-    })
+    # Render an image in the UI output placeholder 'myImage'
+  output$myImage <- renderImage({
+    list(src = "IMG_3156.WEBP",
+         align = "center",
+         width = "100%",
+         height = 500)
+  }, deleteFile = F)
+  
+  # Prepare reactive filtered data based on some conditions
+  filtered_data <-reactive({
+    merged_dataset
+  })
+  
+  # Prepare another set of reactive filtered data based on relationship status and time spent on social media
+  filtered_smmh <-reactive({
+    smmh[smmh$`3. Relationship Status` %in% input$relationship_status &
+           smmh$`8. What is the average time you spend on social media every day?` == input$time_spent, ]
+  })
+  
+  # Render a bar plot in the UI output placeholder 'relationshipPlot'
    output$relationshipPlot <- renderPlot({
     filtered_smmh <- smmh %>%
       filter(`3. Relationship Status` %in% input$relationship_status &
                `8. What is the average time you spend on social media every day?` == input$time_spent)
+   
+     # Create and return a bar plot showing validation seeking behavior by relationship status
     ggplot(filtered_smmh, aes(x = `17. How often do you look to seek validation from features of social media?`, fill = `3. Relationship Status`)) +
       geom_bar(position = "dodge") +
       labs(x = "Frequency of Seeking Validation", y = "Count", title = "Social Media Validation and Usage Time Based on Relationship Status") +
--- ui.R
+++ ui.R
@@ -7,43 +7,49 @@ library(shinythemes)
 
 ## OVERVIEW TAB INFO
 overview_tab <- tabPanel("Introduction",
-  h1("Exploring the Depths of Social Media's Impact"),
-  p("In the digital age, social media has stayed close onto our lives, raising questions about its impact on mental health and well-being. Our project trys to understand the complex relationship between social media usage patterns and their psychological effects. With increasing screen times and horrible habits, understanding these dynamics has never been more important"),
-  p("Our goal is followed by three really important questions, each forming the cornerstone of our analysis. First, we want to understand how age correlates with social media usage and whether certain age groups are more susceptible to its allure. This question aims to shed light on generational differences in social media interaction and its implications."),
-  p("Secondly, we get into the realm of self-perception versus reality by looking into how individuals' self-reported mental health assessments compare with their actual mental health scores. This comparison will hopefully let us understand people and the impact social media has on them."),
-  p("Lastly, we explore the dynamics of validation-seeking behavior on social media. How does one's relationship status influence their pursuit of validation and approval through likes, comments, and shares? This question uncovers the social mechanics at play within these digital interactions."),
-  p("We as a group aim to uncover the nuanced ways in which social media consumption shapes mental health. Join us as we dive deep into the data, trying to find answers that may inform better digital well-being practices and policies."),
-   imageOutput("myImage")                      
-)
+     h1("Introduction"),
+     p("In today's digital age, the impact of social media on mental health is a topic of growing concern. Our project delves into the intricate relationship between social media usage patterns and their effects on psychological well-being."),
+     p("Social media platforms play a significant role in shaping individuals' perceptions of themselves and their surroundings. The constant exposure to curated images and the pursuit of likes can contribute to negative emotions."),
+     p("Our analysis revolves around three key questions: How does the frequency of Social Media usage correlate with age? Is there a correlation between negative mental health and age when using social media? And what is the relationship between the frequency of seeking validation on social media and the duration of usage?"),
+     p("To conduct our research, we utilized datasets from reputable sources. One dataset was obtained from ", a("Mendeley data", href="https://data.mendeley.com/datasets/jxkcm7s638/1"), ", collected by the Kisii University College Faculty of Health Sciences through online surveys and campus data collection methods. Additionally, we utilized a dataset from ",
+     a("Kaggle data", href="https://www.kaggle.com/datasets/souvikahmed071/social-media-and-mental-health"), ", which originated from a university statistics course. This dataset was processed using machine learning algorithms to predict the need for professional help based on social media usage patterns."),
+     p("It's important to note potential biases in our dataset, such as uneven distribution across demographic segments like gender and socioeconomic status. This non-representative sample may skew interpretations of the relationship between social media usage and mental health."),
+     imageOutput("myImage"))
+
+
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 # Page 1 - start
-## VIZ 1 TAB INFO
-# Define UI
-# Visualization 1 Sidebar with Inputs
-
+# The UI definition 
 ui <- navbarPage("Social Medias Affect On Mental Health",
-   overview_tab,
+   overview_tab, # Defined somewhere else group!
+   
+   # The relationship between age, social media usage, and emotion.
    viz_1_tab <- tabPanel("Usage & Emotion",
-     sidebarLayout(
-       sidebarPanel(
-         h2("Age relevancy to Social Media Usage"),
-         sliderInput("ageRange", "Age Range",
-                     min = min(smmh$`1. What is your age?`, na.rm = TRUE), 
-                     max = max(smmh$`1. What is your age?`, na.rm = TRUE), 
-                     value = c(min(smmh$`1. What is your age?`, na.rm = TRUE), max(smmh$`1. What is your age?`, na.rm = TRUE)),
-                     step = 1),
-         selectInput("socialMediaTimeSpent", "Average Time Spent on Social Media",
-                     choices = unique(smmh$`8. What is the average time you spend on social media every day?`))
-       ),
-       mainPanel(
-         h2(),
-         plotOutput("variableDistributionPlot", height = "400px"))
-     )
+       sidebarLayout(
+         sidebarPanel(
+             # Focus on age and social media usage.
+           h2("Age relevancy to Social Media Usage"),
+           # Slider input for selecting an age range. Its DYNAMIC!!
+           sliderInput("ageRange", "Age Range",
+                       min = min(smmh$`1. What is your age?`, na.rm = TRUE), 
+                       max = max(smmh$`1. What is your age?`, na.rm = TRUE), 
+                       value = c(min(smmh$`1. What is your age?`, na.rm = TRUE), max(smmh$`1. What is your age?`, na.rm = TRUE)),
+                       step = 1),
+           # Dropdown to select the average time spent on social media.
+           selectInput("socialMediaTimeSpent", "Average Time Spent on Social Media",
+                choices = unique(smmh$`8. What is the average time you spend on social media every day?`))
+         ),
+         mainPanel(
+           h2(), # What is this? Ask someone!
+           # Output area for a plot.
+           plotOutput("variableDistributionPlot", height = "400px"))
+       )
    ),
+   # End of the first tab's definition.
 )
 
-#  
+
 # Page 1 - end 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -52,27 +58,31 @@ ui <- navbarPage("Social Medias Affect On Mental Health",
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 # Page 2 - begin 
 
+# This one you guys, focuses on the impact of age on mental health in the context of social media use.
 viz_2_tab <- tabPanel("Age-based Impact",
   viz_2_tab <- tabPanel("Age-based Impact",
     fluidPage(
+      # a scatter plot.
       titlePanel("ScatterPlot: Distribution of Mental Health Impact Scores based on social media users"),
       sidebarLayout(
         sidebarPanel(
+          # Provides an option to filter the displayed data by gender.
           h3("Filter by Gender"),
           selectInput("gender", "Gender", choices = c("All", "Male", "Female"), selected = "All"),
+          # Allows us to select which types of scores to display on the plot.
           h3("Select Score Types"),
           checkboxInput("selfReportedCheck", "Self-reported Mental Health Status (Red)", value = TRUE),
           checkboxInput("actualScoreCheck", "Actual Score (Blue)", value = TRUE)
         ),
         mainPanel(
+          # Placeholder for the actual scatter plot visualization.
           plotOutput("plot")
         )
       )
     )
   )
 )
-
-
+# End of the second tab's definition, but watch out for that duplicate definition issue.
 
 
 # Page 2 - end
@@ -83,21 +93,26 @@ viz_2_tab <- tabPanel("Age-based Impact",
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 # Page 3 - begin  
 
-## VIZ 3 TAB INFO
+# social media usage in the context of relationship status.
 viz_3_sidebar <- sidebarPanel(
+  # focus on relationship status and social media usage.
   h2("Social Media Validation Seeking and Daily Usage Time Based on Relationship Status"),
+  # selecting one or more relationship statuses to filter the data.
   checkboxGroupInput("relationship_status", "Relationship Status",
                      choices = c("In a relationship", "Single", "Married", "Divorced"),
                      selected = c("In a relationship", "Single", "Married", "Divorced")),
+  # selecting average time spent on social media
   selectInput("time_spent", "Average Time Spent on Social Media",
               choices = c("Less than an Hour", "Between 1 and 2 hours", "Between 2 and 3 hours", 
                           "Between 3 and 4 hours", "Between 4 and 5 hours", "More than 5 hours")),
 )
 viz_3_main_panel <- mainPanel(
+  #  visualization will be shown from here.
   h2("Vizualization 3: relation plot"),
   plotOutput("relationshipPlot")
 )
 
+#then we have a cookie
 viz_3_tab <- tabPanel("Social Media Engagement by Relationship Status",
   sidebarLayout(
   viz_3_sidebar,
@@ -114,28 +129,20 @@ viz_3_tab <- tabPanel("Social Media Engagement by Relationship Status",
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 # page 4 - start
-
-
-
-
 conclusion_tab <- tabPanel("Our Findings",
-                           h1("Our Findings"),
-                           p("While delving into the  patterns of social media use and its psychological impacts, we uncovered alot of compelling data. 
-                           Starting with the first page of our analysis, which focuses on the correlation between age and social media usage, we found a interesting trend:
-                           a giant portion of individuals, engage with the platform for an average of 4-5 hours daily, with many exceeding 5 hours. 
-                           This extensive use show us a prevailing trend towards heavy consumption. Upon taking a closer look on self-reported mental health assessments, a curious pattern popped up, 
-                           especially among men. Many reported feeling worse about their mental state than what their actual mental health scores suggested. This hints at a complex 
-                           psychological situation where the perception of distress is multiplied beyond the reality of their own mental well-being. Our investigation into social media validation-seeking
-                           behaviors revealed another nuanced dynamic. Married individuals exhibited patterns of seeking validation in short, intense bursts, in contrast to single individuals, who displayed 
-                           a consistent pursuit of validation. This insight paints a bigger picture of social media's grip on users, shwoing us how these platforms can plant and grow negative
-                           psychological traits, such as validation seeking and self-critical judgment. Taking a closer look it becomes very obioues that social media consumption creates a  
-                           relationship with its users thats very negative. Most of the time occupied by individuals dedicating more than 5 hours daily to these platforms, social media not only perpetuates a cycle of mental health 
-                           decline but also ensnares users in a quest for loneliness. The data we've explored paints a sharp picture of a parasitic relationship, where the consumption of social media
-                           becomes a horrible path."))
+    h1("What We've Found"),
+    p("Diving into the world of likes, shares, and endless scrolling, we've come up with some stuff that's definitely worth your attention. For starters, we noticed that a whole lot of people are glued to their screens for 4-5 hours a day, and that's just the average; plenty are clocking in way more."),
+    p("Taking a magnifying glass to the self-rated mental health scene showed us something intriguing. It looks like there's a gap between how bad folks think they're doing and their actual mental health scores—especially for the guys out there. It's like there's this mental filter that's making the bad seem worse."),
+    p("Then there's the hunt for virtual thumbs-ups. Married folks tend to dive into this quest in short, intense bursts, while the single crowd keeps a steady pace. It's a bit of a rollercoaster, showing us just how tangled we can get in the web of social media, chasing after that next hit of digital approval."),
+    p("So, what's the big picture? Spending hours on social media seems to have its downsides—affecting our heads more than some of us might want to admit. It's kind of like a digital parasite, where the more you use, the more you might end up in a not-so-great place, mentally speaking."),
+    p("With all the numbers crunched and the data pored over, our takeaway is this: social media's a double-edged sword, and it's sharp on both sides. It's a space that connects us but also a place that, if we're not careful, can disconnect us from ourselves.")
+)
+
 
 # page 4 - end
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
 
+#declared!!!!!!!!
 
 ui <- navbarPage("Social Medias Affect On Mental Health",
   theme = shinytheme("flatly"),  
